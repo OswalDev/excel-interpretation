156ac0494ca9a1049fabb284c2b946b55a9393d2
Oswaldo Soto <oswaldojsotog@gmail.com>
2023-05-02 17:27:34 -0400

    tailwindcss added

--- src/App.jsx
+++ src/App.jsx
@@ -35,34 +35,39 @@ function App() {
 
     promise.then((d) => {
       setItems(d);
-      console.log(d[0]['ASIGNATURA'])
+      // console.log(d[0]['ASIGNATURA'])
     });
   };
 
   return (
-    <div>
-      <input
-        type="file"
-        onChange={(e) => {
-          const file = e.target.files[0];
-          readExcel(file);
-        }}
-      />
+    <div className="bg-cream">
+      <h1>Excel Data Visualization</h1>
+
+      <div className="">
+        <h3>Insert xlsx file</h3>
+        <input
+          type="file"
+          onChange={(e) => {
+            const file = e.target.files[0];
+            readExcel(file);
+          }}
+        />
+      </div>
 
       <table className="table container">
         <thead>
-          <tr>
+          <tr className="text-center">
             <th scope="col">Item</th>
             <th scope="col">Description</th>
             <th scope="col">GPA</th>
           </tr>
         </thead>
-        <tbody>
+        <tbody className="text-center">
           {slicedArray.map((d) => (
             <tr key={d.ASIGNATURA}>
-              <th>{d.ASIGNATURA}</th>
-              <td>{d.DEFINITIVA}</td>
-              <td>{d.GPA}</td>
+              <th className="text-center">{d.ASIGNATURA}</th>
+              <td className="text-center">{d.DEFINITIVA.toFixed(2)}</td>
+              <td className="text-center">{d.GPA.toFixed(2)}</td>
             </tr>
           ))}
         </tbody>
